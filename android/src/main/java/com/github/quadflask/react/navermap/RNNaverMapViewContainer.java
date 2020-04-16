a2b3016bf9cd9e99e3f7ff914ddddd7564484233
QuadFlask <pop9310@gmail.com>
2020-04-16 12:57:49 +0900

    Fix/clipping view bug (#21)
    Fix #19 
    
    FrameLayout으로 한번 더 감싸고 윈도에 attach/detach 될 때 mapView 생성/제거 처리
    Choreographer 콜백 딜레이 추가
    0.0.47

---
+++ android/src/main/java/com/github/quadflask/react/navermap/RNNaverMapViewContainer.java
@@ -0,0 +1,227 @@
+package com.github.quadflask.react.navermap;
+
+import android.view.Choreographer;
+import android.view.View;
+import android.widget.FrameLayout;
+import androidx.annotation.NonNull;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.LatLngBounds;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class RNNaverMapViewContainer extends FrameLayout implements RNNaverMapViewProps {
+    private final ReactApplicationContext appContext;
+    private final ThemedReactContext themedReactContext;
+    private final FusedLocationSource locationSource;
+    private RNNaverMapView mapView;
+    private RNNaverMapView prevMapView;
+    private boolean isAttachedToWindow = false;
+
+    public RNNaverMapViewContainer(@NonNull ThemedReactContext themedReactContext, ReactApplicationContext appContext, FusedLocationSource locationSource) {
+        super(ReactUtil.getNonBuggyContext(themedReactContext, appContext));
+        this.appContext = appContext;
+        this.themedReactContext = themedReactContext;
+        this.locationSource = locationSource;
+        mapView = new RNNaverMapView(themedReactContext, appContext, locationSource);
+        addView(mapView);
+    }
+
+    // https://github.com/facebook/react-native/issues/17968#issuecomment-457236577
+    private void setupLayoutHack() {
+        Choreographer.getInstance().postFrameCallback(new Choreographer.FrameCallback() {
+            @Override
+            public void doFrame(long frameTimeNanos) {
+                manuallyLayoutChildren();
+                getViewTreeObserver().dispatchOnGlobalLayout();
+                if (isAttachedToWindow)
+                    Choreographer.getInstance().postFrameCallbackDelayed(this, 500);
+            }
+        });
+    }
+
+    private void manuallyLayoutChildren() {
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(MeasureSpec.makeMeasureSpec(getMeasuredWidth(), MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(getMeasuredHeight(), MeasureSpec.EXACTLY));
+            child.layout(0, 0, child.getMeasuredWidth(), child.getMeasuredHeight());
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        isAttachedToWindow = true;
+        if (mapView == null) {
+            mapView = new RNNaverMapView(themedReactContext, appContext, locationSource);
+            mapView.restoreFrom(prevMapView);
+            addView(mapView);
+        }
+        mapView.setId(getId());
+        setupLayoutHack();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        isAttachedToWindow = false;
+        onStop();
+        removeView(mapView);
+        prevMapView = mapView;
+        mapView = null;
+    }
+
+    @Override
+    public void onStop() {
+        if (mapView != null)
+            mapView.onStop();
+    }
+
+    @Override
+    public void setCenter(LatLng latLng) {
+        if (mapView != null)
+            mapView.setCenter(latLng);
+    }
+
+    @Override
+    public void setCenter(LatLng latLng, Double zoom, Double tilt, Double bearing) {
+        if (mapView != null)
+            mapView.setCenter(latLng, zoom, tilt, bearing);
+    }
+
+    @Override
+    public void zoomTo(LatLngBounds latLngBounds, int paddingInPx) {
+        if (mapView != null)
+            mapView.zoomTo(latLngBounds, paddingInPx);
+    }
+
+    @Override
+    public void setTilt(int tilt) {
+        if (mapView != null)
+            mapView.setTilt(tilt);
+    }
+
+    @Override
+    public void setBearing(int bearing) {
+        if (mapView != null)
+            mapView.setBearing(bearing);
+    }
+
+    @Override
+    public void setZoom(float zoom) {
+        if (mapView != null)
+            mapView.setZoom(zoom);
+    }
+
+    @Override
+    public void setMapPadding(int left, int top, int right, int bottom) {
+        if (mapView != null)
+            mapView.setMapPadding(left, top, right, bottom);
+    }
+
+    @Override
+    public void onInitialized() {
+        if (mapView != null)
+            mapView.onInitialized();
+    }
+
+    @Override
+    public void showsMyLocationButton(boolean show) {
+        if (mapView != null)
+            mapView.showsMyLocationButton(show);
+    }
+
+    @Override
+    public void setCompassEnabled(boolean show) {
+        if (mapView != null)
+            mapView.setCompassEnabled(show);
+    }
+
+    @Override
+    public void setScaleBarEnabled(boolean show) {
+        if (mapView != null)
+            mapView.setScaleBarEnabled(show);
+    }
+
+    @Override
+    public void setZoomControlEnabled(boolean show) {
+        if (mapView != null)
+            mapView.setZoomControlEnabled(show);
+    }
+
+    @Override
+    public void setLocationTrackingMode(int mode) {
+        if (mapView != null)
+            mapView.setLocationTrackingMode(mode);
+    }
+
+    @Override
+    public void setMapType(NaverMap.MapType value) {
+        if (mapView != null)
+            mapView.setMapType(value);
+    }
+
+    @Override
+    public void setBuildingHeight(float height) {
+        if (mapView != null)
+            mapView.setBuildingHeight(height);
+    }
+
+    @Override
+    public void setLayerGroupEnabled(String layerGroup, boolean enable) {
+        if (mapView != null)
+            mapView.setLayerGroupEnabled(layerGroup, enable);
+    }
+
+    @Override
+    public void setNightModeEnabled(boolean enable) {
+        if (mapView != null)
+            mapView.setNightModeEnabled(enable);
+    }
+
+    @Override
+    public void setLogoMargin(int left, int top, int right, int bottom) {
+        if (mapView != null)
+            mapView.setLogoMargin(left, top, right, bottom);
+    }
+
+    @Override
+    public void setLogoGravity(int gravity) {
+        if (mapView != null)
+            mapView.setLogoGravity(gravity);
+    }
+
+    @Override
+    public void moveCameraFitBound(LatLngBounds bounds, int left, int top, int right, int bottom) {
+        if (mapView != null)
+            mapView.moveCameraFitBound(bounds, left, top, right, bottom);
+    }
+
+    @Override
+    public void addFeature(View child, int index) {
+        if (mapView != null)
+            mapView.addFeature(child, index);
+    }
+
+    @Override
+    public void removeFeatureAt(int index) {
+        if (mapView != null)
+            mapView.removeFeatureAt(index);
+    }
+
+    @Override
+    public int getFeatureCount() {
+        if (mapView != null)
+            return mapView.getFeatureCount();
+        return 0;
+    }
+
+    @Override
+    public View getFeatureAt(int index) {
+        if (mapView != null)
+            return mapView.getFeatureAt(index);
+        return null;
+    }
+}
---
+++ android/src/main/java/com/github/quadflask/react/navermap/RNNaverMapViewProps.java
@@ -0,0 +1,65 @@
+package com.github.quadflask.react.navermap;
+
+import android.view.View;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.LatLngBounds;
+import com.naver.maps.map.NaverMap;
+
+public interface RNNaverMapViewProps {
+    String[] EVENT_NAMES = new String[]{
+            "onInitialized",
+            "onCameraChange",
+            "onMapClick",
+            "onTouch"
+    };
+
+    void onStop();
+
+    void setCenter(LatLng latLng);
+
+    void setCenter(LatLng latLng, Double zoom, Double tilt, Double bearing);
+
+    void zoomTo(LatLngBounds latLngBounds, int paddingInPx);
+
+    void setTilt(int tilt);
+
+    void setBearing(int bearing);
+
+    void setZoom(float zoom);
+
+    void setMapPadding(int left, int top, int right, int bottom);
+
+    void onInitialized();
+
+    void showsMyLocationButton(boolean show);
+
+    void setCompassEnabled(boolean show);
+
+    void setScaleBarEnabled(boolean show);
+
+    void setZoomControlEnabled(boolean show);
+
+    void setLocationTrackingMode(int mode);
+
+    void setMapType(NaverMap.MapType value);
+
+    void setBuildingHeight(float height);
+
+    void setLayerGroupEnabled(String layerGroup, boolean enable);
+
+    void setNightModeEnabled(boolean enable);
+
+    void setLogoMargin(int left, int top, int right, int bottom);
+
+    void setLogoGravity(int gravity);
+
+    void moveCameraFitBound(LatLngBounds bounds, int left, int top, int right, int bottom);
+
+    void addFeature(View child, int index);
+
+    void removeFeatureAt(int index);
+
+    int getFeatureCount();
+
+    View getFeatureAt(int index);
+}
