fee0f8865890ea07498368057578d02b8a3f11c2
vzts <yeechan.ko@gmail.com>
2020-08-05 21:10:21 +0900

    implement polygon hole for iOS

--- index.js
+++ index.js
@@ -127,10 +127,12 @@ export class Polygon extends Component {
     render() {
         return Platform.select({
             android: () => React.createElement(RNNaverMapPolygonOverlay, Object.assign({}, this.props)),
-            ios: () => React.createElement(RNNaverMapPolygonOverlay, Object.assign({}, this.props, { coordinates: {
+            ios: () => React.createElement(RNNaverMapPolygonOverlay, Object.assign({}, this.props, {
+                coordinates: {
                     exteriorRing: this.props.coordinates,
                     interiorRings: this.props.holes,
-                } }))
+                }
+            })),
         })();
     }
 }
